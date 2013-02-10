1f1e9afa34f9976c12eedc82d93ee0e75e4c4512
Julio Olivera <julio.olvr@gmail.com>
2013-02-10 13:16:20 -0300

    Simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf build;
+middleman build;
+cd ..;
+rm -rf julioolvr.github.com/*;
+cp -r blog/build/* julioolvr.github.com/;
+cd julioolvr.github.com;
+git add -u;
+git ci -m "Blog update";
+git push origin master;
