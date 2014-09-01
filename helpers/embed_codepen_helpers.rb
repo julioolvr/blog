73abaa84d7df739e4089187c166a207aaae5a6d7
Julio Olivera <julio.olvr@gmail.com>
2014-09-01 09:12:18 -0300

    Moved Codepen helper to PostHelpers

--- helpers/embed_codepen_helpers.rb
+++
@@ -1,6 +0,0 @@
-module EmbedCodepenHelpers
-  def codepen(id, options = {})
-    "<p data-height=\"324\" data-theme-id=\"0\" data-slug-hash=\"#{id}\" data-default-tab=\"result\" class=\"codepen\">See the Pen <a href=\"http://codepen.io/julioolvr/pen/#{id}/\">#{id}</a> by joliv (<a href=\"http://codepen.io/julioolvr\">@julioolvr</a>) on <a href=\"http://codepen.io\">CodePen</a>.</p>" +
-    "<script async src=\"//codepen.io/assets/embed/ei.js\"></script>"
-  end
-end
--- helpers/post_helpers.rb
+++ helpers/post_helpers.rb
@@ -4,4 +4,9 @@ module PostHelpers
       link_to "\##{tag}", tag_path(tag)
     end.join(', ')
   end
+
+  def codepen(id, options = {})
+    "<p data-height=\"324\" data-theme-id=\"0\" data-slug-hash=\"#{id}\" data-default-tab=\"result\" class=\"codepen\">See the Pen <a href=\"http://codepen.io/julioolvr/pen/#{id}/\">#{id}</a> by joliv (<a href=\"http://codepen.io/julioolvr\">@julioolvr</a>) on <a href=\"http://codepen.io\">CodePen</a>.</p>" +
+    "<script async src=\"//codepen.io/assets/embed/ei.js\"></script>"
+  end
 end
